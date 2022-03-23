11d5987fe822222eb0b660089cea2b9ecfb95347
Sydney <sydneysirois17@gmail.com>
2022-03-22 20:47:14 -0400

    part-2-answer

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function addNumbers() {
     var firstNumber = document.getElementById('firstNumber').value
     var secondNumber = document.getElementById('secondNumber').value
-    var answer = Number(firstNumber) + Number(secondNumber)
+    var operator = document.getElementById('operator').value
+
+    if (operator === "+") {
+        var answer = Number(firstNumber) + Number(secondNumber);
+
+    } else if (operator === "-") {
+        var answer = Number(firstNumber) - Number(secondNumber);
+    } else if (operator === "*") {
+        var answer = Number(firstNumber) * Number(secondNumber);
+    } else if (operator === "/") {
+        var answer = Number(firstNumber) / Number(secondNumber);
+    }
 
     document.getElementById('answerBox').innerHTML = answer
+}
+
+function clearNumbers() {
+    document.getElementById('firstNumber').value = " "
+    document.getElementById('secondNumber').value = " "
+    document.getElementById('answerBox').innerHTML = " "
 }
